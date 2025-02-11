85d19d0ca875086ec88e2e348a983f853a752c07
seungjae100 <seungjaebaekmail@gmail.com>
2025-02-11 16:21:34 +0900

    security-#1 : admin 계정 확인

---
+++ src/main/java/com/web/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.web.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll() // 모든 사용자 접근 허용
+                        .requestMatchers("/admin").hasRole("ADMIN") // 특정 URL의 접근에 대한 특정 권한자의 접근 허용 (관리자)
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
