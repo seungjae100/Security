9eb854706044b41e77a68bb1a6c78692618c9b27
seungjae100 <seungjaebaekmail@gmail.com>
2025-02-11 17:55:29 +0900

    security-#1 : MySQL 데이터베이스 연동

--- src/main/java/com/web/security/config/SecurityConfig.java
+++ src/main/java/com/web/security/config/SecurityConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean // 비밀번호 암호화 작업
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
